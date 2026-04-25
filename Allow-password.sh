dea60ce0c78c259551b520442147679a09f65902
unknown <danamurthy33@gmail.com>
2026-04-25 20:14:52 +0530

    Practice

--- Allow-password.sh
+++ Allow-password.sh
@@ -2,7 +2,7 @@
 
 P=$1
 
-if [[ $P =~ [a-zA-Z] ]] && [[ $P =~ [0-9] ]] && [[ $P =~ [~!@#$%^&*] ]]; then
+if [[[ $P =~ [a-zA-Z] ]] && [[ $P =~ [0-9] ]] && [[ $P =~ [~!@#$%^&*] ]]]; then
     echo "$P is strong password"
 else
     echo "$P is weak password"
