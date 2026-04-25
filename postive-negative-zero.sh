4bee4236be890e40c59712bfa754cb2369893b4b
unknown <danamurthy33@gmail.com>
2026-04-25 20:12:57 +0530

    Practice

--- postive-negative-zero.sh
+++ postive-negative-zero.sh
@@ -2,9 +2,9 @@
 
 N=$1
 
-if (N>0); then
+if ((N > 0)); then
     echo "$N is a Postive Number"
-elif (N<0); then
+elif ((N < 0)); then
     echo "$N is a Negtive Number"
 else
     echo "$N is Zero"
