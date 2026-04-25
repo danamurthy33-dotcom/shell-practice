a08835e0f7ab3ea847801b4444f100d6e6fff84a
unknown <danamurthy33@gmail.com>
2026-04-26 03:20:02 +0530

    Scripts

--- Date.sh
+++ Date.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-S = $(date +%F)
+S=$(date +%F)
 
 echo "Todays date is $S"
 
-Start_Time = $(date +%S)
+Start_Time=$(date +%S)
 sleep 10
-End_Time = $(date +%S)
-Total = $(($End_Time - $Start_Time ))
+End_Time=$(date +%S)
+Total=$(($End_Time - $Start_Time ))
 echo "Execution time is $Total"
