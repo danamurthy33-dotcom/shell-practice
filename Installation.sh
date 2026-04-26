8582cdd2695033c1e52206c30f6fcaa33e991da3
unknown <danamurthy33@gmail.com>
2026-04-27 01:01:49 +0530

    script

--- Installation.sh
+++ Installation.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 ID=$(id -u)
-[ "$ID" -ne 0 ] && echo "Run as root" && exit 1
+#[ "$ID" -ne 0 ] && echo "Run as root" && exit 1
+if (($ID == 0)); then
+    echo "Error:Please run as root user"
+    exit 1
+else
+    exit 0
+fi
+
 
 dnf install mysql -y
