b6eb7ca1eb75424839e1b698ffc1fc6a9b1144ad
unknown <danamurthy33@gmail.com>
2026-04-27 03:22:36 +0530

    script

--- Installation.sh
+++ Installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ID=$(id -u)
-LogFolder="/var/log/shellscript-logs.logs"
+LogFolder="/var/log/shellscript-logs"
 Script=$(echo $0 | cut -d "." -f1)
 LogFile="$LogFolder/$Script.log"
-mkdir -p $LogFolder
+mkdir -p "$LogFolder"
 #[ "$ID" -ne 0 ] && echo "Run as root" && exit 1
 if (( $ID != 0 )); then
     echo -e "\e[31m Error\e[0m:Please run as root user"
@@ -12,11 +12,11 @@ if (( $ID != 0 )); then
 fi
 Validate (){
     #&>/dev/null for cheing the command silently
-dnf list installed "$1" &>>LogFile
+dnf list installed "$1" &>>"$LogFile"
 if [ $? -eq 0 ]; then
     echo -e "$1 is already \e[32m installed\e[0m"
 else
-    dnf install "$1" -y &>>LogFile
+    dnf install "$1" -y &>>"$LogFile"
     if [ $? -ne 0 ]; then
         echo -e "Error:\e[33m installing \e[0m $1"
         exit 1
